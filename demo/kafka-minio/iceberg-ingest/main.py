5ee3eb68f79e0564699ef9daece0aa02cd364b44
bachthetrollface <bachthetrollface@gmail.com>
2025-11-10 15:57:27 +0700

    update code to use hadoop catalog

--- demo/kafka-minio/iceberg-ingest/main.py
+++ demo/kafka-minio/iceberg-ingest/main.py
@@ -14,10 +14,10 @@ def get_schema():
     ])
 
 def create_iceberg_table(spark: SparkSession):
-    spark.sql("""CREATE NAMESPACE IF NOT EXISTS my_catalog.demo""")
+    spark.sql("""CREATE NAMESPACE IF NOT EXISTS my_catalog.cleaned""")
     
     spark.sql("""
-        CREATE TABLE IF NOT EXISTS my_catalog.demo.raw_events (
+        CREATE TABLE IF NOT EXISTS my_catalog.cleaned.events (
             id string,
             value double,
             source string,
@@ -29,21 +29,19 @@ def create_iceberg_table(spark: SparkSession):
     # add partitioning
 
 def get_spark_session():
-    catalog_uri = os.getenv("CATALOG_URI", "http://iceberg-rest:8181")
+    # catalog_uri = os.getenv("CATALOG_URI", "http://iceberg-rest:8181")
+    warehouse_path = os.getenv("CATALOG_WAREHOUSE", "s3a://warehouse/")
     return (
         SparkSession.builder
             .appName("minio-to-iceberg")
         
             # Catalog configs
-            # simply need to config connection to catalog, other configs in the catalog's config (in container, ...)
             .config("spark.sql.catalog.my_catalog", "org.apache.iceberg.spark.SparkCatalog")
-            .config("spark.sql.catalog.my_catalog.type", "rest")
-            .config("spark.sql.catalog.my_catalog.uri", catalog_uri)
-            # .config("spark.sql.catalog.my_catalog.warehouse", "s3a://warehouse/")
+            .config("spark.sql.catalog.my_catalog.type", "hadoop")
+            # .config("spark.sql.catalog.my_catalog.uri", catalog_uri)
+            .config("spark.sql.catalog.my_catalog.warehouse", warehouse_path)
             .config("spark.sql.catalog.my_catalog.io-impl", "org.apache.iceberg.aws.s3.S3FileIO")
-            # .config("spark.sql.catalog.my_catalog.default-namespace", "demo")
             .config("spark.sql.defaultCatalog", "my_catalog")
-            
             .config("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions")
             
             # MinIO S3 configs (for s3a interface)
@@ -51,6 +49,7 @@ def get_spark_session():
             .config("spark.hadoop.fs.s3a.access.key", "minio")
             .config("spark.hadoop.fs.s3a.secret.key", "minio123")
             .config("spark.hadoop.fs.s3a.path.style.access", "true")
+            .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
             
             .getOrCreate()
     )
@@ -68,13 +67,16 @@ raw_df = (
         .schema(raw_data_schema)    # define the schema manually
         .load(f"s3a://{raw_zone_path}/*") # for Spark to track subdirectories of partitioned files
 )
+
+# ops here for basic data cleaning
+
 query = (
     raw_df.writeStream
         .format("iceberg")
         .outputMode("append")
-        .trigger(processingTime="1 minute") # write period
+        .trigger(processingTime="15 seconds") # write period
         .option("checkpointLocation", "s3a://checkpoints/iceberg-ingest/")
-        .toTable("my_catalog.demo.raw_events")
+        .toTable("my_catalog.cleaned.events")
 )
 
 try:
@@ -84,3 +86,4 @@ except KeyboardInterrupt:
     print("Streaming to Iceberg stopped.")
 finally:
     query.awaitTermination()
+    spark.stop()
