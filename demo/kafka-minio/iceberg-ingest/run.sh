8897b3ed6568b3902cc261dacba10bcea5b495db
bachthetrollface <bachthetrollface@gmail.com>
2025-12-04 23:57:41 +0700

    implement Nessie catalog for Iceberg

--- demo/kafka-minio/iceberg-ingest/run.sh
+++ demo/kafka-minio/iceberg-ingest/run.sh
@@ -1,6 +1,5 @@
 mkdir -p /tmp/.ivy2 && /opt/spark/bin/spark-submit \
     --master local[*] \
-    --packages \
-    org.apache.iceberg:iceberg-spark-runtime-3.5_2.12:1.10.0,org.apache.hadoop:hadoop-aws:3.3.4,com.amazonaws:aws-java-sdk-bundle:1.12.262 \
     --conf spark.jars.ivy=/tmp/.ivy2 \
+    --packages org.apache.iceberg:iceberg-spark-runtime-3.5_2.12:1.10.0,org.projectnessie.nessie-integrations:nessie-spark-extensions-3.5_2.12:0.104.5,org.apache.hadoop:hadoop-aws:3.3.4,com.amazonaws:aws-java-sdk-bundle:1.12.262 \
     /app/main.py
